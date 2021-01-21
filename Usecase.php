fb4346a0ceefaf3f38ed1d1417f9773226e96506
Seta-art <tollystephane@gmail.com>
2021-01-21 17:54:21 +0100

    Improving the display formatting on the usecase

--- Usecase.php
+++ Usecase.php
@@ -6,12 +6,19 @@
 
 //NB : First argument is the class(GINF2,...), the second one is the category(eleve, professeur,note,module)
 //Respect the format and be sure that the corresponding data exist in "resource" folder
+
 require_once "ExcelToXml.php";
 
 $App=new ExcelToXml();
 
-$xml=$App->Excel2Xml("GINF2","eleve");
-var_dump($xml);
+$xml=$App->Excel2Xml("GINF2","module");
+
+$dom = new DOMDocument("1.0");
+$dom->preserveWhiteSpace = false;
+$dom->formatOutput = true;
+$dom->loadXML($xml->asXML());
+echo "<pre>".htmlentities($dom->saveXML())."</pre>"
+
 
 
 ?>
