9986c65483aec7e899a3e9008c96a5703403a777
Seta-art <tollystephane@gmail.com>
2021-01-21 17:38:49 +0100

    Generate SimpleXml for GINF2 students

--- Usecase.php
+++ Usecase.php
@@ -4,8 +4,14 @@
 //It shows a usecase of Excel2Xml ; a class, we will be using in our project for converting Excel File to SimpleXml
 
 
+//NB : First argument is the class(GINF2,...), the second one is the category(eleve, professeur,note,module)
+//Respect the format and be sure that the corresponding data exist in "resource" folder
+require_once "ExcelToXml.php";
 
+$App=new ExcelToXml();
 
+$xml=$App->Excel2Xml("GINF2","eleve");
+var_dump($xml);
 
 
 ?>
