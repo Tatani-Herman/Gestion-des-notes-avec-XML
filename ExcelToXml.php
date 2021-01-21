427abe6a48811ef0075dfec191b0a7e26cd243c8
Seta-art <tollystephane@gmail.com>
2021-01-21 17:30:37 +0100

    Filling the class ExcelToXml; Check the usecase later

---
+++ ExcelToXml.php
@@ -0,0 +1,82 @@
+<?php
+require 'vendor/autoload.php';  
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
+
+//See the use case for better undestanding of how it works
+//The main method is Excel2Xml() : it takes $class( ex: GINF2 ) and $categorie(ex : eleve/professeur/note/module)
+//as argument and return a SimpleXml instance
+class ExcelToXml 
+{
+
+    public function Excel2Xml($class,$categorie)
+    {
+    $objReader = IOFactory::createReader('Xlsx');
+        $file="resources/".$class."/".$categorie.".xlsx";
+        $objPHPExcel = $objReader->load($file);
+       
+    
+        $index = 0;
+        $data = Array();
+        foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+    
+            $objPHPExcel->setActiveSheetIndex($index);
+            
+            $sheetData = $worksheet->toArray(null, true, true, true);
+            
+            $result = Array();
+            $keys = $sheetData[1];
+            for ($i = 2; $i < count($sheetData); $i++) {
+                $rowValue = Array();
+                $row = $sheetData[$i];
+                foreach($row as $key=>$value) {
+                    if ($keys[$key] != "") {
+                        $rowValue[$keys[$key]] = $value;
+                    }
+                }
+                for ($j = 0; $j < count($row); $j++) {
+                    
+                }
+                $result[] = $rowValue;
+            }
+            
+            $data[str_replace(array("-"," "), "_", $worksheet->getTitle())] = $result;
+            $index++;
+        }
+        
+        $xml = $this->array2xml($data, $class,false);
+        return $xml;
+
+    }
+
+    private function array2xml($array,$root,$xml = false){
+
+        
+        
+        if($xml === false){
+            $base='<'.$root.'/>';
+            $xml = new SimpleXMLElement($base);
+        }
+        foreach($array as $key => $value){
+            if(is_array($value)){
+                if( is_numeric($key) ){
+                    $key = 'item'.$key; //dealing with <0/>..<n/> issues
+                }
+                $this->array2xml($value,$key,$xml->addChild($key));
+            } else {
+                if($value){
+                    $xml->addChild($key, htmlspecialchars($value));
+                }
+               
+            }
+        }
+    
+        //return $xml->asXML();
+        return $xml;
+    }
+
+}
+
+
+?>
