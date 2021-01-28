6403fd1cce399432aafeba49b72b4f152071e392
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2021-01-28 11:41:33 +0100

    xsd files

--- dashboard.php
+++ dashboard.php
@@ -4,10 +4,21 @@
 
     <div class="main_content">
       <div class="header">
-        Bienvenue sur votre tableau de bord chèr(e) <?php echo $login; ?>
+        <h3>Bienvenue sur votre tableau de bord chèr(e) Admin</h3>
       </div>
       <div class="info">
-        taxte
+     <div> Vous pouvez effectuez les opérations suivantes grace à cette plateforme </div>
+      <div class="list-group">
+  
+  <a href="#" class="list-group-item list-group-item-action list-group-item-primary">Consulter les résultats du modules programmation orientée objet & XML</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">Générer les cartes d'étudiant</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-success">Générer des emploi du temps</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-danger">Générer les relever de notes des étudiants de GINF2</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-warning">Générer le procès verbal de la filière GINF2</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-info">Générer des attestation de réussite</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-light">Elaborer des liste de groupe de TPs</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">This is a dark list group item</a>
+</div>
       </div>
     </div>
   </div>
--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <link rel="icon" href="img/index.ico"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
   </head>
   <body>
@@ -14,15 +15,17 @@
    <input type="checkbox" id="check">
     <!--header area start-->
     <header>
-      <label for="check">
-        <i class="fas fa-bars" id="sidebar_btn"></i>
-      </label>
+  
+      <nav class="navbar navbar-dark bg-dark">
       <div class="left_area">
         <h3>ENSA <span>Tanger</span></h3>
       </div>
-      <div class="right_area">
-        <a href="index.php" class="logout_btn">Logout</a>
-      </div>
+      <label for="check">
+        <i class="fas fa-bars" id="sidebar_btn"></i>
+      </label>
+      <a href="index.php" class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
+</nav>
+
     </header>
     <!--header area end-->
     <!--sidebar start-->
