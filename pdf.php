e993e11ebd75570b00d0c5959fa8ce66d95465a8
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2021-01-29 17:41:09 +0100

    lescture et telechargement de la carte

--- pdf.php
+++ pdf.php
@@ -1,23 +1,46 @@
 <?php
-require("HTTPPost.php");
+/*require("HTTPPost.php");
 
 // input xml data
-$xml="<?xml version=\"1.0\" encoding=\"iso-8859-1\"?> <departments><department><departmentName>R&amp;D</departmentName> <person> <name>John Schmidt</name> ";
-$xml=$xml . "<address>Red street 3</address> <status>A</status> </person> <person> <name>Paul Bones</name> <address>White street 5</address> <status>A</status> ";
-$xml=$xml . "</person> <person> <name>Mark Mayer</name> <address>Blue street 5</address> <status>A</status> </person> <person> <name>Janet Black</name> ";
-$xml=$xml . "<address>Black street 8</address> <status>I</status> </person></department><department> <departmentName>Sales</departmentName> <person> ";
-$xml=$xml . "<name>Juan Gomez</name> <address>Green street 3</address> <status>A</status> </person> <person> <name>Juliet Bones</name> <address>White street 5</address> ";
-$xml=$xml . " <status>A</status> </person></department></departments>";
 $xml2=simplexml_load_file ("xmlResources/carte.xml");
 // make request to FOP server
 $httppost=new HTTPPost();
-$pdfdata=$httppost->post_request("localhost","80","C://xampp/htdocs/Gestion-des-notes-avec-XML/xsl_foFiles/carte.fo",$xml2);
+$pdfdata=$httppost->post_request("localhost","8087","C:\xampp\htdocs\Gestion-des-notes-avec-XML\xsl_foFiles\carte.fo",$xml2);
 
 // save PDF output to a PDF file
 $myFile = "testFile.pdf";
 $fh = fopen($myFile, 'w') or die("can't open file");
 fwrite($fh, $pdfdata);
-fclose($fh);
+fclose($fh);*/
 
+
+// Stocker le nom du fichier dans une variable
+$file = 'carte.pdf'; 
+    
+// Type de contenu dd l'en-tête
+header('Content-type: application/pdf'); 
+  
+header('Content-Disposition: inline; filename="' . $file . '"'); 
+  
+header('Content-Transfer-Encoding: binary'); 
+  
+header('Accept-Ranges: bytes'); 
+  
+// Lire le fichier
+@readfile($file); 
+
+
+function forcerTelechargement($nom, $situation, $poids)
+{
+  header('Content-Type: application/octet-stream');
+  header('Content-Length: '. $poids);
+  header('Content-disposition: attachment; filename='. $nom);
+  header('Pragma: no-cache');
+  header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+  header('Expires: 0');
+  readfile($situation);
+  exit();
+}
+forcerTelechargement('carte.pdf', 'carte.pdf',filesize('carte.pdf'));
 
 ?>
